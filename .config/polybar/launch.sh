cc164fd997855a02630318006e510d44ed3758bb
sleepntsheep <contact@papangkorn.com>
2021-12-15 12:32:21 +0700

    splitbar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,5 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar -q main -c "$DIR"/config.ini &
+    MONITOR=$m polybar -q center -c "$DIR"/config.ini &
+    MONITOR=$m polybar -q left -c "$DIR"/config.ini &
+    MONITOR=$m polybar -q right -c "$DIR"/config.ini &
 done
