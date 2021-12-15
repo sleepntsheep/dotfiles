81ae86d6e5ef0f6966be396211afd8a20977213e
sleepntsheep <contact@papangkorn.com>
2021-12-15 18:43:02 +0700

    rofipower

--- .config/polybar/powermenu.sh
+++ .config/polybar/powermenu.sh
@@ -1,21 +1,13 @@
-#!/usr/bin/env bash
-
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
-
-dir="~/.config/polybar/grayblocks/scripts/rofi"
+dir="~/.config/polybar/rofi"
 uptime=$(uptime -p | sed -e 's/up //g')
 
-rofi_command="rofi -theme $dir/powermenu.rasi"
+rofi_command="rofi -theme ~/.config/polybar/powermenu.rasi"
 
-# Options
-shutdown=" Shutdown"
-reboot=" Restart"
-lock=" Lock"
-suspend=" Sleep"
-logout=" Logout"
+shutdown="Shutdown"
+reboot="Restart"
+lock="Lock"
+suspend="Sleep"
+logout="Logout"
 
 # Confirmation
 confirm_exit() {
@@ -34,7 +26,7 @@ msg() {
 # Variable passed to rofi
 options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
 
-chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
+chosen="$(echo -e "$options" | $rofi_command -p "" -dmenu -selected-row 0)"
 case $chosen in
     $shutdown)
 		ans=$(confirm_exit &)
