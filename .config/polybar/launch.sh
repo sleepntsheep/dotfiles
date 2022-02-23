d6c686744bc8b612cd251f39df8df258e04c8417
sleepntsheep <sleepntsheep@gmail.com>
2022-02-23 12:30:52 +0700

    brightnesshotkey

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,7 +4,7 @@ DIR="$HOME/.config/polybar"
 
 killall -q polybar
 
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
 #    MONITOR=$m polybar -q center -c "$DIR"/config.ini &
